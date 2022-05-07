7b7cc1da8fe27c5a46e6c5521e5429ed4af380ee
Pushpendra9350 <pushpendra@Pushpendras-MacBook-Air.local>
2022-05-07 23:00:03 +0530

    updatesAdded

--- SaveAndSearch/settings.py
+++ SaveAndSearch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s^8^4qruo84g78#t^a=6^675!@=1qh-zhn#=3(@0v3*ff%hr8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saveandsearch.herokuapp.com','0.0.0.0','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
