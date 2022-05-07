1817a56ab2ea16dc969bb57096954f1402862e8d
Pushpendra9350 <pushpendra@Pushpendras-MacBook-Air.local>
2022-05-07 23:04:13 +0530

    secretkey removed

--- SaveAndSearch/settings.py
+++ SaveAndSearch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s^8^4qruo84g78#t^a=6^675!@=1qh-zhn#=3(@0v3*ff%hr8i'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
